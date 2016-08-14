a39614f17ce0a76854870462432f87b4f39332d7
AlMel3000 <AlMel@live.com>
2016-08-14 20:35:07 +0300

    in app billing redesigned UI
    in app billing - with stubs for a while

--- app/src/main/java/org/hr24/almel/ResumeBuilder/ui/fragments/MainFragment.java
+++ app/src/main/java/org/hr24/almel/ResumeBuilder/ui/fragments/MainFragment.java
@@ -1,6 +1,8 @@
 package org.hr24.almel.ResumeBuilder.ui.fragments;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
@@ -12,8 +14,10 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 
+import com.crashlytics.android.Crashlytics;
 import com.github.gorbin.asne.core.SocialNetwork;
 import com.github.gorbin.asne.core.SocialNetworkManager;
 import com.github.gorbin.asne.core.listener.OnLoginCompleteListener;
@@ -24,9 +28,13 @@ import com.vk.sdk.util.VKUtil;
 
 import org.hr24.almel.ResumeBuilder.R;
 import org.hr24.almel.ResumeBuilder.ui.StartActivity;
+import org.hr24.almel.ResumeBuilder.util.IabBroadcastReceiver;
+import org.hr24.almel.ResumeBuilder.util.IabHelper;
+import org.hr24.almel.ResumeBuilder.util.IabResult;
+import org.hr24.almel.ResumeBuilder.util.Inventory;
+import org.hr24.almel.ResumeBuilder.util.Purchase;
 import org.hr24.almel.ResumeBuilder.utils.ConstantManager;
 import org.hr24.almel.ResumeBuilder.utils.NetworkStatusChecker;
-
 import java.util.List;
 
 import ru.ok.android.sdk.util.OkScope;
@@ -39,20 +47,34 @@ import ru.ok.android.sdk.util.OkScope;
  * Use the {@link MainFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class MainFragment extends Fragment implements SocialNetworkManager.OnInitializationCompleteListener, OnLoginCompleteListener, View.OnClickListener {
+public class MainFragment extends Fragment implements SocialNetworkManager.OnInitializationCompleteListener, OnLoginCompleteListener, View.OnClickListener, IabBroadcastReceiver.IabBroadcastListener {
 
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
+    static final String TAG = "inAppBilling";
+
     Button vkButton, okButton, fillButton, premiumButton;
     CoordinatorLayout mCoordinatorLayout;
     LinearLayout authLinLayout;
     View fillView;
+    ImageView logoImageView, mailImageView, callImageView;
     public static SocialNetworkManager mSocialNetworkManager;
     int networkId = 0;
     public static boolean AUTHORIZATION_STATUS = false;
     public static boolean PREMIUM_STATUS = false;
+
+    // SKUs for our products: the premium upgrade (non-consumable)
+    static final String SKU_PREMIUM = "premium";
+
+    static final int RC_REQUEST = 10001;
+
+    // The helper object
+    IabHelper mHelper;
+
+    // Provides purchase notification while this app is running
+    IabBroadcastReceiver mBroadcastReceiver;
 
 
     private String mParam1;
@@ -105,10 +127,16 @@ public class MainFragment extends Fragment implements SocialNetworkManager.OnIni
         authLinLayout = (LinearLayout) rootView.findViewById(R.id.auth_ll);
         fillView = rootView.findViewById(R.id.fill_v);
         premiumButton = (Button) rootView.findViewById(R.id.premium_btn);
+        logoImageView = (ImageView) rootView.findViewById(R.id.logo_iv);
+        mailImageView = (ImageView) rootView.findViewById(R.id.mail_iv);
+        callImageView = (ImageView) rootView.findViewById(R.id.call_iv);
 
         vkButton.setOnClickListener(this);
         okButton.setOnClickListener(this);
         fillButton.setOnClickListener(this);
+        logoImageView.setOnClickListener(this);
+        mailImageView.setOnClickListener(this);
+        callImageView.setOnClickListener(this);
         premiumButton.setOnClickListener(this);
 
         String VK_KEY = getActivity().getString(R.string.vk_app_id);
@@ -122,8 +150,7 @@ public class MainFragment extends Fragment implements SocialNetworkManager.OnIni
 
         String[] vkScope = new String[] {
                 VKScope.WALL,
-                VKScope.NOHTTPS,
-                VKScope.STATUS,
+                VKScope.NOHTTPS
         };
 
 
@@ -161,19 +188,239 @@ public class MainFragment extends Fragment implements SocialNetworkManager.OnIni
         initStatus();
 
         if (AUTHORIZATION_STATUS||PREMIUM_STATUS){
-            authLinLayout.setVisibility(View.GONE);
-            fillView.setVisibility(View.GONE);
+           updateUi();
         }
 
+        //region ============ inAppBilling ===========
 
+
+     /*   *//* base64EncodedPublicKey should be YOUR APPLICATION'S PUBLIC KEY
+         * (that you got from the Google Play developer console). This is not your
+         * developer public key, it's the *app-specific* public key.
+         *
+         * Instead of just storing the entire literal string here embedded in the
+         * program,  construct the key at runtime from pieces or
+         * use bit manipulation (for example, XOR with some other string) to hide
+         * the actual key.  The key itself is not secret information, but we don't
+         * want to make it easy for an attacker to replace the public key with one
+         * of their own and then fake messages from the server.
+         *//*
+        String base64EncodedPublicKey = "CONSTRUCT_YOUR_KEY_AND_PLACE_IT_HERE";
+
+
+        Log.d(TAG, "Creating IAB helper.");
+        mHelper = new IabHelper(getContext(), base64EncodedPublicKey);
+
+
+        //// TODO: 14.08.16  enable debug logging (for a production application, you should set this to false).
+        mHelper.enableDebugLogging(true);
+
+        // Start setup. This is asynchronous and the specified listener
+        // will be called once setup completes.
+        Log.d(TAG, "Starting setup.");
+        mHelper.startSetup(new IabHelper.OnIabSetupFinishedListener() {
+            public void onIabSetupFinished(IabResult result) {
+                Log.d(TAG, "Setup finished.");
+
+                if (!result.isSuccess()) {
+                    // Oh noes, there was a problem.
+                    complain("Problem setting up in-app billing: " + result);
+                    return;
+                }
+
+                // Have we been disposed of in the meantime? If so, quit.
+                if (mHelper == null) return;
+
+                // Important: Dynamically register for broadcast messages about updated purchases.
+                // We register the receiver here instead of as a <receiver> in the Manifest
+                // because we always call getPurchases() at startup, so therefore we can ignore
+                // any broadcasts sent while the app isn't running.
+                // Note: registering this listener in an Activity is a bad idea, but is done here
+                // because this is a SAMPLE. Regardless, the receiver must be registered after
+                // IabHelper is setup, but before first call to getPurchases().
+                mBroadcastReceiver = new IabBroadcastReceiver(MainFragment.this);
+                IntentFilter broadcastFilter = new IntentFilter(IabBroadcastReceiver.ACTION);
+                getContext().registerReceiver(mBroadcastReceiver, broadcastFilter);
+
+                // IAB is fully set up. Now, let's get an inventory of stuff we own.
+                Log.d(TAG, "Setup successful. Querying inventory.");
+                try {
+                    mHelper.queryInventoryAsync(mGotInventoryListener);
+                } catch (IabHelper.IabAsyncInProgressException e) {
+                    complain("Error querying inventory. Another async operation in progress.");
+                    Crashlytics.logException(e);
+                }
+            }
+        });*/
+
+        //endregion
 
         return rootView;
     }
+
+    // Listener that's called when we finish querying the items and subscriptions we own
+    IabHelper.QueryInventoryFinishedListener mGotInventoryListener = new IabHelper.QueryInventoryFinishedListener() {
+        public void onQueryInventoryFinished(IabResult result, Inventory inventory) {
+            Log.d(TAG, "Query inventory finished.");
+
+            // Have we been disposed of in the meantime? If so, quit.
+            if (mHelper == null) return;
+
+            // Is it a failure?
+            if (result.isFailure()) {
+                complain("Failed to query inventory: " + result);
+                return;
+            }
+
+            Log.d(TAG, "Query inventory was successful.");
+
+            /*
+             * Check for items we own. Notice that for each purchase, we check
+             * the developer payload to see if it's correct! See
+             * verifyDeveloperPayload().
+             */
+
+            // Do we have the premium upgrade?
+            Purchase premiumPurchase = inventory.getPurchase(SKU_PREMIUM);
+            PREMIUM_STATUS = (premiumPurchase != null && verifyDeveloperPayload(premiumPurchase));
+            Log.d(TAG, "User is " + (PREMIUM_STATUS ? "PREMIUM" : "NOT PREMIUM"));
+
+            if (PREMIUM_STATUS){updateUi();}
+            Log.d(TAG, "Initial inventory query finished; enabling main UI.");
+        }
+    };
+
+    @Override
+    public void receivedBroadcast() {
+        // Received a broadcast notification that the inventory of items has changed
+        Log.d(TAG, "Received broadcast notification. Querying inventory.");
+        try {
+            mHelper.queryInventoryAsync(mGotInventoryListener);
+        } catch (IabHelper.IabAsyncInProgressException e) {
+            complain("Error querying inventory. Another async operation in progress.");
+            Crashlytics.logException(e);
+        }
+    }
+
+    // User clicked the "Upgrade to Premium" button.
+    public void onUpgradeAppButtonClicked() {
+        Log.d(TAG, "Upgrade button clicked; launching purchase flow for upgrade.");
+
+        /* TODO: for security, generate your payload here for verification. See the comments on
+         *        verifyDeveloperPayload() for more info. Since this is a SAMPLE, we just use
+         *        an empty string, but on a production app you should carefully generate this. */
+        String payload = "";
+
+        try {
+            mHelper.launchPurchaseFlow(getActivity(), SKU_PREMIUM, RC_REQUEST,
+                    mPurchaseFinishedListener, payload);
+        } catch (IabHelper.IabAsyncInProgressException e) {
+            complain("Error launching purchase flow. Another async operation in progress.");
+            Crashlytics.logException(e);
+        }
+    }
+
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        Log.d(TAG, "onActivityResult(" + requestCode + "," + resultCode + "," + data);
+        if (mHelper == null) return;
+
+        // Pass on the activity result to the helper for handling
+        if (!mHelper.handleActivityResult(requestCode, resultCode, data)) {
+            // not handled, so handle it ourselves (here's where you'd
+            // perform any handling of activity results not related to in-app
+            // billing...
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+        else {
+            Log.d(TAG, "onActivityResult handled by IABUtil.");
+        }
+    }
+
+    /** Verifies the developer payload of a purchase. */
+    boolean verifyDeveloperPayload(Purchase p) {
+        String payload = p.getDeveloperPayload();
+
+        /*
+         * TODO: verify that the developer payload of the purchase is correct. It will be
+         * the same one that you sent when initiating the purchase.
+         *
+         * WARNING: Locally generating a random string when starting a purchase and
+         * verifying it here might seem like a good approach, but this will fail in the
+         * case where the user purchases an item on one device and then uses your app on
+         * a different device, because on the other device you will not have access to the
+         * random string you originally generated.
+         *
+         * So a good developer payload has these characteristics:
+         *
+         * 1. If two different users purchase an item, the payload is different between them,
+         *    so that one user's purchase can't be replayed to another user.
+         *
+         * 2. The payload must be such that you can verify it even when the app wasn't the
+         *    one who initiated the purchase flow (so that items purchased by the user on
+         *    one device work on other devices owned by the user).
+         *
+         * Using your own server to store and verify developer payloads across app
+         * installations is recommended.
+         */
+
+        return true;
+    }
+
+    // Callback for when a purchase is finished
+    IabHelper.OnIabPurchaseFinishedListener mPurchaseFinishedListener = new IabHelper.OnIabPurchaseFinishedListener() {
+        public void onIabPurchaseFinished(IabResult result, Purchase purchase) {
+            Log.d(TAG, "Purchase finished: " + result + ", purchase: " + purchase);
+
+            // if we were disposed of in the meantime, quit.
+            if (mHelper == null) return;
+
+            if (result.isFailure()) {
+                complain("Error purchasing: " + result);
+                return;
+            }
+            if (!verifyDeveloperPayload(purchase)) {
+                complain("Error purchasing. Authenticity verification failed.");
+                return;
+            }
+
+            Log.d(TAG, "Purchase successful.");
+
+           if (purchase.getSku().equals(SKU_PREMIUM)) {
+                // bought the premium upgrade!
+                Log.d(TAG, "Purchase is premium upgrade. Congratulating user.");
+                alert("Спасибо за получение премиум-статуса!");
+                PREMIUM_STATUS = true;
+                updateUi();
+
+            }
+
+        }
+    };
+
+
 
     @Override
     public void onPause() {
         super.onPause();
         saveStatus();
+        //region ============ inAppBilling ===========
+
+       /* // very important:
+        if (mBroadcastReceiver != null) {
+            getContext().unregisterReceiver(mBroadcastReceiver);
+        }
+
+        // very important:
+        Log.d(TAG, "Destroying helper.");
+        if (mHelper != null) {
+            mHelper.disposeWhenFinished();
+            mHelper = null;
+        }*/
+
+        //endregion
+
 
     }
 
@@ -201,8 +448,8 @@ public class MainFragment extends Fragment implements SocialNetworkManager.OnIni
                     break;
             }
 
-            authLinLayout.setVisibility(View.GONE);
-            fillView.setVisibility(View.GONE);
+            updateUi();
+
             AUTHORIZATION_STATUS = true;
             saveStatus();
 
@@ -210,6 +457,10 @@ public class MainFragment extends Fragment implements SocialNetworkManager.OnIni
         }
     }
 
+    private void updateUi() {
+        authLinLayout.setVisibility(View.GONE);
+        fillView.setVisibility(View.GONE);
+    }
 
 
     @Override
@@ -275,13 +526,35 @@ public class MainFragment extends Fragment implements SocialNetworkManager.OnIni
                 }
                 break;
             case R.id.premium_btn:
+                // onUpgradeAppButtonClicked();
                 PREMIUM_STATUS = true;
                 saveStatus();
-                authLinLayout.setVisibility(View.GONE);
-                fillView.setVisibility(View.GONE);
-                showSnackbar("Эта функиональность пока не работает, поэтому премиум БЕСПЛАТНОесплатно");
+                updateUi();
+                showSnackbar("Эта функиональность пока не работает, поэтому премиум БЕСПЛАТНО");
 
                 break;
+            case R.id.logo_iv:
+                Intent viewIntent = new Intent(Intent.ACTION_VIEW);
+                viewIntent.setData(Uri.parse("http://hr24.org"));
+                startActivity(viewIntent);
+                break;
+
+            case R.id.mail_iv:
+                Intent sendIntent = new Intent(Intent.ACTION_SENDTO);
+                sendIntent.setData(Uri.parse("mailto:info@hr24.org"));
+                sendIntent.putExtra(Intent.EXTRA_SUBJECT, "From ResumeBuilder app");
+                startActivity(sendIntent);
+
+                break;
+
+            case R.id.call_iv:
+                Intent callIntent = new Intent(Intent.ACTION_DIAL);
+                callIntent.setData(Uri.parse("tel:+ 7 925 721 21 68"));
+                startActivity(callIntent);
+
+
+                break;
+
 
         }
 
@@ -307,8 +580,7 @@ public class MainFragment extends Fragment implements SocialNetworkManager.OnIni
     public void onLoginSuccess(int socialNetworkID) {
         StartActivity.hideProgress();
         showSnackbar("Успешная авторизация");
-        authLinLayout.setVisibility(View.GONE);
-        fillView.setVisibility(View.GONE);
+        updateUi();
         AUTHORIZATION_STATUS = true;
         saveStatus();
 
@@ -361,5 +633,18 @@ public class MainFragment extends Fragment implements SocialNetworkManager.OnIni
         AUTHORIZATION_STATUS = StartActivity.getSharedPref().getBoolean(ConstantManager.AUTHORIZATION_STATUS_KEY, false);
         PREMIUM_STATUS = StartActivity.getSharedPref().getBoolean(ConstantManager.PREMIUM_STATUS_KEY, false);
 
+    }
+
+    void complain(String message) {
+        Log.e(TAG,  message);
+        alert("Ошибка: " + message);
+    }
+
+    void alert(String message) {
+        AlertDialog.Builder bld = new AlertDialog.Builder(getContext());
+        bld.setMessage(message);
+        bld.setNeutralButton("OK", null);
+        Log.d(TAG, "Showing alert dialog: " + message);
+        bld.create().show();
     }
 }
